8cf144cdf2ac952eff20cf4bc5b4a99898a5093d
ReelManifesto <brian.lesnak@gmail.com>
2023-12-04 10:20:27 -0500

    Code gives an output but yields the wrong sum

--- 2023/day3_gearRatios/day3_gearRatios.py
+++ 2023/day3_gearRatios/day3_gearRatios.py
@@ -13,8 +13,9 @@ with open('input.txt') as input:
 
 # print(partList)
 # Loop through partList lines to find numbers.
+i = 0
 for line in partList:
-    i = 0
+    j = 0
     
     # Find all the numbers in the current line in the order they appear
     numbers = re.findall(r'\d+',line)
@@ -24,7 +25,6 @@ for line in partList:
 
     # Search lines for digit for start/end of numbers
     for character in line:
-        j = 0
         if skipDigit == 0 and character == numbers[numberIndex][0]:
             # print("Found Number: " + str(numbers[numberIndex]))
             skipDigit = int(math.log10(int(numbers[numberIndex])))+1
@@ -32,6 +32,27 @@ for line in partList:
             # print(character)
 
             # TODO: Insert loop to look for symbols at partList[i][j] +- 1 in all directions based on skipDigit
+            # Define vertical bounds of symbol search to not exceed size of "partList"
+            iLowerBound = 0 if (i - 1 < 0) else (i - 1)
+            iUpperBound = len(partList) if (i +1 > len(partList)) else (i + 1)
+            
+            # Define horizontal bounds of symbol search to not exceed size of "line"
+            jLowerBound = 0 if (j - 1 < 0) else (i - 1)
+            jUpperBound = len(line) if (i + 1 > len(line)) else (j + 1)
+            
+            # Check for symbol matches within the bounds defined around the number
+            symbols = "!@#$%^&*()-+?_=,<>/"
+            match = False
+            for iIndex in range(iLowerBound, iUpperBound+1):
+                if match is False:
+                    for jIndex in range(jLowerBound, jUpperBound+1):
+                        if (partList[iIndex][jIndex] in symbols):
+                            print("Match! Adding " + str(numbers[numberIndex]) + " to sumParts (" + str(sumParts) + ")")
+                            match = True
+                            sumParts += int(numbers[numberIndex])
+                            break
+                
+
 
             skipDigit -= 1
             if numberIndex < len(numbers) - 1:
@@ -39,4 +60,8 @@ for line in partList:
         elif skipDigit != 0:
             # print(character)
             skipDigit -= 1
-            
+
+        j += 1
+    i += 1
+
+print("Parts Sum = " + str(sumParts))
